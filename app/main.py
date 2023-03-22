7ac46aea07ae23c24b9b5217494b5e596f0a2db1
JuneAekkalak <aekkalak.ju@gmail.com>
2023-03-22 13:54:01 +0700

    fix error

--- app/main.py
+++ app/main.py
@@ -11,9 +11,9 @@ def read_root():
 def read_name(name: str = None):
     return {"hello": name}
 
-@app.post("/callname/")
+@app.post("/callname")
 def post_name():
-    name = "aaa"
+    name = "june"
     return {"hello": name}
 
 
